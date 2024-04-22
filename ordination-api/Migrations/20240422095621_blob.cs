3635f89ac5021a30b77aa46e1cf588df49aec6d8
Casper Emil Bjødstrup <eaa23ceb@students.eaaa.dk>
2024-04-22 12:07:04 +0200

    mirgrationes time

---
+++ ordination-api/Migrations/20240422095621_blob.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ordination_api.Migrations
+{
+    public partial class blob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

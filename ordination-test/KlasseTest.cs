ea43220bafddc57595155fa4ccc70a4ebb106bf6
Amelia Valentin <eaaalov@students.eaaa.dk>
2024-04-25 11:49:03 +0200

    Test1

---
+++ ordination-test/KlasseTest.cs
@@ -0,0 +1,55 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordination_test
+{
+    [TestClass]
+
+    public class KlasseTest
+
+    {
+        private DataService service;
+
+        [TestInitialize]
+        public void SetupBeforeEachTest()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+            optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+            var context = new OrdinationContext(optionsBuilder.Options);
+            service = new DataService(context);
+            service.SeedData();
+        }
+
+        [TestMethod]
+        public void BeregnDoegnDosisForDagligFast()
+        {
+            // Arrange
+            Patient patient = service.GetPatienter().First();
+            Laegemiddel lm = service.GetLaegemidler().First();
+            DateTime startDato = DateTime.Now;
+            DateTime slutDato = DateTime.Now.AddDays(3);
+            double morgenAntal = 2;
+            double middagAntal = 2;
+            double aftenAntal = 1;
+            double natAntal = 0;
+
+            // Opret en instans af DagligFast
+            DagligFast dagligFast = new DagligFast(startDato, slutDato, lm, morgenAntal, middagAntal, aftenAntal, natAntal);
+
+            // Act
+            double forventetDoegnDosis = morgenAntal + middagAntal + aftenAntal + natAntal;
+            double beregnetDoegnDosis = dagligFast.doegnDosis();
+
+            // Assert
+            Assert.AreEqual(forventetDoegnDosis, beregnetDoegnDosis);
+        }
+
+    }
+}
--- ordination-test/PatientTest.cs
+++ ordination-test/PatientTest.cs
@@ -12,20 +12,10 @@ public class PatientTest
         string cpr = "160563-1234";
         string navn = "John";
         double vægt = 83;
-        
+
         Patient patient = new Patient(cpr, navn, vægt);
         Assert.AreEqual(navn, patient.navn);
     }
 
 
-    [TestMethod]
-    public void TestDerAltidFejler()
-    {
-        string cpr = "160563-1234";
-        string navn = "John";
-        double vægt = 83;
-
-        Patient patient = new Patient(cpr, navn, vægt);
-        Assert.AreEqual("Egon", patient.navn);
-    }
 }

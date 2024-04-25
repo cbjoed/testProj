7de33191519bb0e2740c928f0ab9815e04470f69
Casper Emil Bjødstrup <eaa23ceb@students.eaaa.dk>
2024-04-25 13:14:49 +0200

    casper test

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -41,21 +41,62 @@ public class ServiceTest
         Assert.AreEqual(2, service.GetDagligFaste().Count());
     }
 
+
     [TestMethod]
-    public void OpretDagligSkæv()
+    public void OpretDagligSkaev()
     {
-        ///TODO
+        
+        int patientId = 1; 
+        int laegemiddelId = 1; 
+        Dosis[] doser = new Dosis[] { }; 
+        DateTime startDato = DateTime.Now;
+        DateTime slutDato = DateTime.Now.AddDays(3);
+
+        
+        DagligSkæv result = service.OpretDagligSkaev(patientId, laegemiddelId, doser, startDato, slutDato);
+
+        
+        Assert.IsNotNull(result); 
     }
+
     [TestMethod]
     public void OpretPN()
     {
         ///TODO
     }
+
     [TestMethod]
     public void VægtKategori()
     {
-        ///TODO
+        
+        int patientId = 1; 
+        int laegemiddelId = 1; 
+
+        
+        double recommendedDose = service.GetAnbefaletDosisPerDøgn(patientId, laegemiddelId);
+
+      
+        Assert.IsTrue(recommendedDose == 9.51, "Recommended dose should be 9.51");
     }
+    [TestMethod]
+    public void SøgPatient()
+    {
+        
+        string cprNumberToSearch = "121256-0512";
 
+        
+        var patients = service.GetPatienter();
 
+        
+        bool patientFound = false;
+        foreach (var patient in patients)
+        {
+            if (patient.cprnr == cprNumberToSearch)
+            {
+                patientFound = true;
+                break;
+            }
+        }
+
+    }
 }
